1ad5d976e5029f94f95a67cf80011d0dfe921cb4
Himanshu Mishra <thomas.aparna1512@gmail.com>
2024-02-25 22:17:55 -0500

    Test

--- SA/Stock_price.py
+++ SA/Stock_price.py
@@ -61,7 +61,7 @@ if company_name:
 
         # Convert index to datetime and localize timezone
         tickerDF.index = pd.to_datetime(tickerDF.index)
-        tickerDF.index = tickerDF.index.tz_localize("EST").tz_convert("UTC")
+        tickerDF.index = tickerDF.index.tz_localize("America/New_York").tz_convert("UTC")
 
         # Create two columns for side-by-side display
         col1, col2 = st.columns(2)
